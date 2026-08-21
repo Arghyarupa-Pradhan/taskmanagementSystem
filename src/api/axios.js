0bca13150c1fa3658176a1b3a7cfb7136cb52005
Arghyarupa-Pradhan <arghyarupa03543@gmail.com>
2026-08-21 10:33:12 +0530

    Fix backend API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskmanagement-backend-4vwp.onrender.com/api",
+  baseURL: "https://taskmanagement-backend-4ywp.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
